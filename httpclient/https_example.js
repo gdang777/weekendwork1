fa740899311dd0467fed30df635112ea84f5f0e0
Gaurav <netgary7@gmail.com>
2018-09-30 20:27:01 +0000

    Adding the https file

--- httpclient/https_example.js
+++ httpclient/https_example.js
@@ -1,2 +1,19 @@
 var https = require('https');
+
 console.log("i did it")
+
+var options = {
+    host: 'www.example.org',
+    path: '/'
+
+};
+var callback = function(){
+    console.log("In response handler call back");
+
+}
+console.log("I m about to make the request;");
+https.request(options, callback).end();
+
+console.log("Ive made the request!!");
+
+
